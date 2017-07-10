4e66113285a7012737213a48c9a752f576b7315b
Chris Chapman <cwchapman1@gmail.com>
2017-07-10 23:22:17 +0700

    Add button change

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector("img");
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute("src");
+    if (mySrc === "http://iconizer.net/files/3D_Cartoons_III/orig/Mozilla%20Firefox.png") {
+        myImage.setAttribute("src", "http://megaicons.net/static/img/icons_title/44/116/title/app-firefox-icon.png");
+    } else {
+        myImage.setAttribute("src", "http://megaicons.net/static/img/icons_title/44/116/title/app-firefox-icon.png");
+    }
+}
