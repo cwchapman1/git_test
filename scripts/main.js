4e97cb4ef60673dcbe451d0945a2abe66e8e8889
Chris Chapman <cwchapman1@gmail.com>
2017-07-10 23:23:30 +0700

    Button edit

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,6 @@ myImage.onclick = function() {
     if (mySrc === "http://iconizer.net/files/3D_Cartoons_III/orig/Mozilla%20Firefox.png") {
         myImage.setAttribute("src", "http://megaicons.net/static/img/icons_title/44/116/title/app-firefox-icon.png");
     } else {
-        myImage.setAttribute("src", "http://megaicons.net/static/img/icons_title/44/116/title/app-firefox-icon.png");
+        myImage.setAttribute("src", "http://iconizer.net/files/3D_Cartoons_III/orig/Mozilla%20Firefox.png");
     }
 }
